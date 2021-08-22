7a4b70094dd97f758a2b145423dca79aff4d39d4
Olivier Labrecque-lacasse <olabrecq@c2r4p3.42quebec.com>
2021-08-22 11:39:42 -0400

    another one

--- array/duplicateZero.c
+++ array/duplicateZero.c
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 15:37:18 by olabrecq          #+#    #+#             */
-/*   Updated: 2021/08/19 13:22:07 by olabrecq         ###   ########.fr       */
+/*   Updated: 2021/08/22 11:39:15 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,57 +21,24 @@ Explanation: After calling your function, the input array is modified to: [1,0,0
 #include <stdio.h>
 #include <stdlib.h>
 
-void duplicateZeros(int *arr, int arrSize)
+void ft_shift(int *arr, int position, int arrSize)
 {
     int i;
-    int position;
-    int nb_zero;
+    
+    i = 0;
+    while (arrSize --> position)
+    {
+        arr[i] = arr[i - 1];       
+    }
+}
 
+void duplicateZeros(int* arr, int arrSize)
+{
+    int i;
+    
     i = 0;
     while (i < arrSize)
     {
-        
+        i += arr[i] ? 1 : (ft_shift(arr, i + 1, arrSize), 2);
     }
-} 
-
-int main()
-{
-    int array[100] = {0};
-    int i;
-    int x;
-    int position;
-    int length;
-    int nb_pos;
-
-    i = 0;
-    length = 10;
-    while (i < length)
-    {
-        array[i] = i + 1;
-        printf("%d,", array[i]);
-        i++;
-    }
-    nb_pos = 0;
-    printf("\n");
-    x = 50;
-    position = 5;
-    nb_pos++;
-    length++; /* si on veux ajouter un element , mais pas si on veux shift */
-    //shift
-    i = length;
-    while (i >= position)
-     {
-        array[i] = array[i - 1];
-        i--; 
-    }
-    array[position - 1] = x;
-    //print array 
-    i = 0;
-    while (i < length - nb_pos)
-    {
-        printf("%d,", array[i]);
-        i++;
-    }
-    printf("\n");
-    return (0);
 }
